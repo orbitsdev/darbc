22dbf225315a09157a0fcf4b26dbd619b2ffa241
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-02-13 22:53:46 +0800

    uplaod files

---
+++ resources/js/pages/login.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+    <div class="bg-green-700 py-4">
+            Login
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

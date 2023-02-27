e3f6eced64ffc383e15f13ff8fd261d93f84779c
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-02-27 09:55:35 +0800

    update

--- public/build/assets/reports-17325876.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./adminlayout-4e9e89b0.js";import{_ as a,c as t,a as r,r as n,o as s}from"./app-7d6558ab.js";const c={layout:e};function p(l,_,m,d,f,i){const o=n("DarTable");return s(),t("div",null,[r(o)])}const x=a(c,[["render",p]]);export{x as default};

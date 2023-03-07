7bdbba94d73de248a95766a39bbe938e50bdf215
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-03-07 11:58:03 +0800

    update uii

---
+++ public/build/assets/calendar-231ef9bd.js
@@ -0,0 +1,1 @@
+import{a as e}from"./adminlayout-c3a78cf6.js";import{_ as o,c as t,a as r,r as n,o as s}from"./app-d333e7e0.js";const c={layout:e},l={class:"bg-white p-6"};function _(p,d,i,m,u,f){const a=n("DarFullCalendar");return s(),t("div",l,[r(a)])}const x=o(c,[["render",_]]);export{x as default};

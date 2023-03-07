b896a53292a24744bb8aedb16c6fd06028644173
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-03-07 10:59:32 +0800

    npm ruin build chnage

---
+++ public/build/assets/calendar-67894599.js
@@ -0,0 +1,1 @@
+import{a as e}from"./adminlayout-02df1f36.js";import{_ as o,c as t,a as r,r as n,o as s}from"./app-11c3a7c1.js";const c={layout:e},l={class:"bg-white p-6"};function _(p,d,i,m,u,f){const a=n("DarFullCalendar");return s(),t("div",l,[r(a)])}const x=o(c,[["render",_]]);export{x as default};
